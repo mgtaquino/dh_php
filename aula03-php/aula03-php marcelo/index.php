ee9cc6617411a71c440bffa12d78065ff09cda8e
marcelo aquino <mgtaquino@ymail.com>
2019-07-03 19:31:31 -0300

    adding aula 3 php

---
+++ aula03-php/aula03-php marcelo/index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+  <link rel="stylesheet" href="assets/css/style.css">
+  <title>Document</title>
+</head>
+<body>
+
+  <!-- variaveis de conteudo -->
+  <?php
+
+   ?>
+
+  <main>
+    <!-- navBar -->
+    <nav class="navbar navbar-default">
+      <div class="container-fluid">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">
+            <span>Cursos</span>
+          </a>
+        </div>
+        <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+            <ul class="nav navbar-nav">
+              <li class="dropdown">
+                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Ações <span class="caret"></span></a>
+                <ul class="dropdown-menu">
+                  <li><a href="#">Adicionar Produto</a></li>
+                  <li><a href="#">Editar Produto</a></li>
+                </ul>
+              </li>
+            </ul>
+        </div>
+      </div>
+    </nav>
+    <!-- produtos -->
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-4 col-sm-6 col-md-6 col-lg-6">
+          <div class="thumbnail">
+            <img src="assets/img/full.jpeg" alt="Full Stack">
+            <div class="caption">
+              <h3>Full Stack</h3>
+              <p>Curso de desenvolvimento web</p>
+              <p><strong>R$ 49.99</strong></p>
+              <a href="#" class="btn btn-primary" role="button">Comprar</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-xs-4 col-sm-6 col-md-6 col-lg-6">
+          <div class="thumbnail">
+            <img src="assets/img/full.jpeg" alt="Full Stack">
+            <div class="caption">
+              <h3>Marketing Digital</h3>
+              <p>Curso de marketing digital</p>
+              <p><strong>R$ 52.99</strong></p>
+              <a href="#" class="btn btn-primary" role="button">Comprar</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js" charset="utf-8"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+</body>
+</html>
